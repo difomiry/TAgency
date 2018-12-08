f849c7f29bb3bb72b836d5b4c02145cf04da255b
difomiry <difomiry@gmail.com>
2018-12-08 18:43:53 +0300

    Added PathToDatabase property to settings

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -1,7 +1,7 @@
 ﻿namespace TAgency.Properties
 {
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
     {
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -11,6 +11,21 @@
             get
             {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("TAgency.db")]
+        public string PathToDatabase
+        {
+            get
+            {
+                return ((string)(this["PathToDatabase"]));
+            }
+            set
+            {
+                this["PathToDatabase"] = value;
             }
         }
     }
