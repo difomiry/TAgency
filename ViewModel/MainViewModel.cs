5bd40b26022167602c396e6b49a3cdb8003402ca
difomiry <difomiry@gmail.com>
2018-12-08 18:34:18 +0300

    Fixed stub for PathToDatabase property

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -8,6 +8,19 @@ namespace TAgency.ViewModel
     public class MainViewModel : ViewModelBase
     {
         /// <summary>
+        /// The path to database.
+        /// </summary>
+        private string pathToDatabase = "undefined";
+
+        /// <summary>
+        /// The path to database.
+        /// </summary>
+        public string PathToDatabase
+        {
+            get => pathToDatabase;
+            set => Set(ref pathToDatabase, value);
+        }
+        /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
